1bc8ed4042a00c5f491b54161afea66cc3e448b1
saubun <dameon0430@gmail.com>
2021-04-21 21:23:31 -0400

    first

---
+++ build/CMakeFiles/learnSFML.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learnSFML.dir/main.cpp.o"
+  "CMakeFiles/learnSFML.dir/main.cpp.o.d"
+  "learnSFML"
+  "learnSFML.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learnSFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/dameon0430/Documents/Dev/SFML/learno
+# Build directory: /Users/dameon0430/Documents/Dev/SFML/learno/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
